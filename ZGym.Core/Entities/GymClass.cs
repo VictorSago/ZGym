8df8b572b0ce41be42ea88127e87c006e5e3e1b1
Victor Sago <VictorSago01@gmail.com>
2021-04-05 15:32:41 +0200

    First scaffolding of Identity pages

---
+++ ZGym.Core/Entities/GymClass.cs
@@ -0,0 +1,19 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace ZGym.Core.Entities
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Description { get; set; }
+        
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        
+        // public virtual ICollection<ApplicationUserGymClass> AttendingMembers { get; set; }
+    }
+}
---
+++ ZGym.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ZGym.Data.Data;
+
+[assembly: HostingStartup(typeof(ZGym.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ZGym.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
