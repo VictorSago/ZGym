18ff9964fd9b31dd584b38439dc29bd2daae3ff3
Victor Sago <VictorSago01@gmail.com>
2021-04-06 12:01:19 +0200

    Extra demo

--- ZGym.Web/Controllers/HomeController.cs
+++ ZGym.Web/Controllers/HomeController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ZGym.Core.Entities;
+using ZGym.Data.Data;
 using ZGym.Web.Models;
 
 namespace ZGym.Web.Controllers
@@ -12,14 +15,19 @@ namespace ZGym.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;                   // Extra: Not Needed
+        private readonly UserManager<ApplicationUser> _userManager;         // Extra: Not Needed
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _dbContext = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            var loggedInUser = _userManager.GetUserId(User);
             return View();
         }
 
