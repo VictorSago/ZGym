5cbf1884cc8cc32f89963dfe4438fa5c7f24ab47
Victor Sago <VictorSago01@gmail.com>
2021-04-06 15:40:55 +0200

    Seeding complete

--- ZGym.Web/Program.cs
+++ ZGym.Web/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ZGym.Data.Data;
 
 namespace ZGym.Web
 {
@@ -13,7 +16,33 @@ namespace ZGym.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                // context.Database.EnsureDeleted();
+                context.Database.Migrate();
+
+                // dotnet user-secrets set "AdminPW" "LexiconNA21!"
+                var config = services.GetRequiredService<IConfiguration>();
+
+                var adminPW = config["AdminPW"];
+
+                try
+                {
+                    SeedData.InitAsync(services, adminPW).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex.Message, "Seed Fail");
+                    throw;
+                }
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- ZGym.Web/Startup.cs
+++ ZGym.Web/Startup.cs
@@ -34,11 +34,17 @@ namespace ZGym.Web
             
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            services.AddDefaultIdentity<ApplicationUser>(options => 
+                        {
+                            options.SignIn.RequireConfirmedAccount = false;
+                            options.Password.RequireNonAlphanumeric = false;
+                        })
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             
             services.AddControllersWithViews();
+
+            // services.AddControllersWithViews(config => );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
